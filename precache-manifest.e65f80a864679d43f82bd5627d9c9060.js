a5d697cd0be1d9dbdd50ab657b29dd756381c361
ardato belay <aradatobelay@gmaill.com>
2020-06-14 22:40:29 +0300

    Updates

---
+++ precache-manifest.e65f80a864679d43f82bd5627d9c9060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e913a3a876dfe66214972998823f47",
+    "url": "/TaskManager/index.html"
+  },
+  {
+    "revision": "1658595c6c9cb1559e44",
+    "url": "/TaskManager/static/css/main.d03d513b.chunk.css"
+  },
+  {
+    "revision": "595f245ee47a7c2c7e32",
+    "url": "/TaskManager/static/js/2.df79e183.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TaskManager/static/js/2.df79e183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1658595c6c9cb1559e44",
+    "url": "/TaskManager/static/js/main.58b01cdb.chunk.js"
+  },
+  {
+    "revision": "788131ecd7ce05d125a5",
+    "url": "/TaskManager/static/js/runtime-main.9096db15.js"
+  }
+]);
